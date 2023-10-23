299fb2360dd09d7d4abfd252309ee593fe48c24f
Samith Ravishana <samithucsc3@gmail.com>
2023-10-23 07:44:42 +0000

    configure backend project

--- app-backend/backend/Startup.cs
+++ app-backend/backend/Startup.cs
@@ -7,14 +7,31 @@ namespace backend
 {
     public class Startup
     {
+        public IServiceCollection Configuration;
+        readonly string allowedOrogins = "allowedOrigins";
+
+
         public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => 
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins(Configuration["AllowedOrigins"]?.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+                                                         .AllowAnyHeader()
+                                                         .WithMethods("PUT", "GET", "POST");
+                                  });
+            }};
+
             services.AddControllers();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
